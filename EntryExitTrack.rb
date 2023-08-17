3314dad72b62171f4ed2d3f18113d2cc8c43ebbc
Koji Hinooka <k.hinooka@qtech-i.jp>
2023-08-17 12:00:44 +0900

    feat:入館時刻出力追加, docs:README.mdを追加 refactor:Excel書込処理の関数化

--- EntryExitTrack.rb
+++ EntryExitTrack.rb
@@ -3,10 +3,10 @@ require 'date'
 require 'rubyXL'
 require 'rubyXL/convenience_methods'
 
-def entry_exit_track
+def read_entry_exit_csv
   data = {}
-  # output.csvは除いて処理する
-  Dir.glob('./*.csv').reject{|f| f == './output.csv'}.each do |file|
+  # "output"で始まるcsvファイルは除いて処理する
+  Dir.glob('./*.csv').reject{|f| f =~ /^\.\/output.*\.csv$/}.each do |file|
     CSV.foreach(file, headers: true, skip_lines: /^1/).with_index do |row, idx|
       # next if idx.zero? # 1行目は無視
       next if row['氏名'].nil? || row['氏名'].strip.empty? # "氏名"が空の行は無視
@@ -39,7 +39,8 @@ end
 
 def output_csv(data)
   # 結果の出力(タブ区切りのCSVファイル)
-  CSV.open('output.csv', 'wb', col_sep: "\t") do |csv|
+  filename = "output_#{Time.now.strftime('%Y%m%d%H%M%S')}.csv"
+  CSV.open(filename, 'wb', col_sep: "\t") do |csv|
     csv << ['名前', '入館日時', '退館日時']
     data.sort.each do |name, dates|
       dates.sort.each do |date, times|
@@ -48,75 +49,49 @@ def output_csv(data)
       end
     end
   end
-
-  # 結果の出力
-  # data.each do |date, names|
-  #   puts "日付: #{date}"
-  #   names.each do |name, times|
-  #     puts "氏名: #{name} -> 最初: #{times[:earliest]}, 最後: #{times[:latest]}"
-  #   end
-  #   puts '-' * 30
-  # end
 end
 
-data = entry_exit_track
-# output_csv(data)
+def extry_exit_track(data)
+  Dir.glob('./*.xlsx').each do |excel_file_path|
 
-Dir.glob('./*.xlsx').each do |excel_file_path|
-
-  # Excelファイルを開く
-  workbook = RubyXL::Parser.parse(excel_file_path)
-
-  data.sort.each do |name, dates|
-    # 対応する名前のシートを探す
-    worksheet = workbook[name.delete("　").delete(" ")]
-    # シートが存在しない場合は次のループへ
-    next unless worksheet
-
-    worksheet.each do |row|
-      break if row.nil? || row.r > 40 # とりあえず40行目まで
-      next if row.cells.nil? || row.cells[0].nil? || row.cells[0].value.nil?
-
-      cell_date = Date.new(1900, 1, 1) + row.cells[0].value.to_i - 2
-
-      date_value = dates[cell_date.strftime('%Y/%m/%d')]
-
-      if date_value
-        # 日付が一致する行が見つかった場合、G列(インデックス6)に退館時刻を設定
-        # worksheet.add_cell(row.r + 1, 6, cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:latest])
-
-        # Excelの日付形式での日時の値を計算
-        # datetime = DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:latest], "%Y/%m/%d %H:%M:%S")
-        # excel_datetime = datetime.ajd - Date.new(1899, 12, 30).ajd
-
-        cell = worksheet.add_cell(row.r - 1, 6)
-
-        # セルの書式を時間形式に設定
-        cell.set_number_format('h:mm')
-
-        cell.change_contents(DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:latest], "%Y/%m/%d %H:%M:%S"))
+    # Excelファイルを開く
+    workbook = RubyXL::Parser.parse(excel_file_path)
+  
+    data.sort.each do |name, dates|
+      # 対応する名前のシートを探す
+      worksheet = workbook[name.delete("　").delete(" ")]
+      # シートが存在しない場合は次のループへ
+      next unless worksheet
+  
+      worksheet.each do |row|
+        break if row.nil? || row.r > 40 # とりあえず40行目まで
+        next if row.cells.nil? || row.cells[0].nil? || row.cells[0].value.nil?
+  
+        # 日付が一致する行が見つかった場合、F列(インデックス5)に入館時刻、G列(インデックス6)に退館時刻を設定
+        cell_date = Date.new(1900, 1, 1) + row.cells[0].value.to_i - 2
+        date_value = dates[cell_date.strftime('%Y/%m/%d')]
+  
+        if date_value
+          # 入館時刻を設定
+          cell = worksheet.add_cell(row.r - 1, 5)
+          # セルの書式を時間形式に設定
+          cell.set_number_format('h:mm')
+          # セルの値を設定
+          cell.change_contents(DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:earliest], "%Y/%m/%d %H:%M:%S"))
+  
+          # 退館時刻を設定
+          cell = worksheet.add_cell(row.r - 1, 6)
+          cell.set_number_format('h:mm')
+          cell.change_contents(DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:latest], "%Y/%m/%d %H:%M:%S"))
+        end
       end
     end
+  
+    # Excelファイルを上書き保存
+    workbook.write(excel_file_path)
   end
-
-  # Excelファイルを上書き保存
-  workbook.write(excel_file_path)
 end
 
-# # 実行ファイルのディレクトリを取得
-# current_directory = File.dirname(__FILE__)
-
-# # 現在のディレクトリ内のすべてのCSVファイルを取得
-# csv_files = Dir.glob(File.join(current_directory, "*.csv"))
-
-# # 各CSVファイルの内容をShift-JISからUTF-8に変換
-# csv_files.each do |csv_file|
-#   # ファイルをShift-JISとして読み込み
-#   content = File.open(csv_file, 'r:Shift_JIS') { |f| f.read }
-
-#   # 文字コードを変換。無効なバイトシーケンスや変換できない文字は「?」に置き換える
-#   utf8_content = content.encode('UTF-8', 'Shift_JIS', invalid: :replace, undef: :replace)  
-
-#   # ファイルにUTF-8として上書き保存
-#   File.write(csv_file, utf8_content)
-# end
+data = read_entry_exit_csv
+output_csv(data)
+extry_exit_track(data)
