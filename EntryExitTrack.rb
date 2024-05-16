1362309f2d8acf222f1d9bbeb26a14aeb712dfdc
Koji Hinooka <kohzih@gmail.com>
2024-05-16 23:30:09 +0900

    fix: 入退館時間の初期値設定の修正

--- EntryExitTrack.rb
+++ EntryExitTrack.rb
@@ -6,6 +6,9 @@ require 'rubyXL/convenience_methods'
 DATE_CELL_INDEX = 0
 ENTRY_TIME_CELL_INDEX = 5
 EXIT_TIME_CELL_INDEX = 6
+# 機器アドレスの下2桁が"02"なら入館、"03"なら退館
+ENTRY_CODE = '02'
+EXIT_CODE = '03'
 
 def read_entry_exit_csv
   data = {}
@@ -14,7 +17,7 @@ def read_entry_exit_csv
   Dir.glob('./*.csv').each do |file|
     # ファイル名の先頭が"input_"または"summary_"で始まるファイルは処理対象外とする
     file_name = File.basename(file)  # ファイルの名前だけを取得
-    next if file_name.start_with?('input_') || file_name.start_with?('summary_')
+    next if file_name.start_with?('input_', 'summary_')
 
     convert_and_trim_file(file)
   end
@@ -29,15 +32,17 @@ def read_entry_exit_csv
       time = row['日時'].split(' ').last  # hh:mm:ss の部分だけ取得
 
       data[date] ||= {}
-      data[date][row['氏名']] ||= { earliest: '23:59:59', latest: '00:00:00' }
+      data[date][row['氏名']] ||= { earliest: '__:__:__', latest: '__:__:__' }
 
-      # 機器アドレスの下2桁が"02"なら入館、"03"なら退館と判断
-      if row['機器アドレス'][-2..-1] == "02"
+      # 機器アドレスの下2桁で入館、退館を判断
+      if row['機器アドレス'][-2..-1] == ENTRY_CODE
         # 一番早い時刻には入館時刻のみを設定
-        data[date][row['氏名']][:earliest] = [data[date][row['氏名']][:earliest], time].min
-      elsif row['機器アドレス'][-2..-1] == "03"
+        earliest_time = data[date][row['氏名']][:earliest]
+        data[date][row['氏名']][:earliest] = earliest_time == '__:__:__' ? time : [earliest_time, time].min
+      elsif row['機器アドレス'][-2..-1] == EXIT_CODE
         # 一番遅い時刻には退館時刻のみを設定
-        data[date][row['氏名']][:latest] = [data[date][row['氏名']][:latest], time].max
+        latest_time = data[date][row['氏名']][:latest]
+        data[date][row['氏名']][:latest] = latest_time == '__:__:__' ? time : [latest_time, time].max
       end
     end
     # 読み込んだCSVファイルを削除する
@@ -114,19 +119,16 @@ def extry_exit_track(data)
         cell_date = Date.new(1900, 1, 1) + row.cells[DATE_CELL_INDEX].value.to_i - 2
         date_value = dates[cell_date.strftime('%Y/%m/%d')]
 
-        if date_value
-          # 入館時刻を設定
-          cell = worksheet.add_cell(row.r - 1, ENTRY_TIME_CELL_INDEX)
-          # セルの書式を時間形式に設定
-          cell.set_number_format('h:mm')
-          # セルの値を設定
-          cell.change_contents(DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:earliest], "%Y/%m/%d %H:%M:%S"))
+        next if date_value.nil? # 日付が一致するデータがない場合は次のループへ
 
-          # 退館時刻を設定
-          cell = worksheet.add_cell(row.r - 1, EXIT_TIME_CELL_INDEX)
-          cell.set_number_format('h:mm')
-          cell.change_contents(DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value[:latest], "%Y/%m/%d %H:%M:%S"))
-
+        # 入館時刻を設定
+        if date_value[:earliest] != '__:__:__'
+          set_time_in_cell(worksheet, row, ENTRY_TIME_CELL_INDEX, date_value[:earliest], cell_date)
+          updated = true
+        end
+        # 退館時刻を設定
+        if date_value[:latest] != '__:__:__'
+          set_time_in_cell(worksheet, row, EXIT_TIME_CELL_INDEX, date_value[:latest], cell_date)
           updated = true
         end
       end
@@ -135,6 +137,12 @@ def extry_exit_track(data)
     # Excelファイルを上書き保存
     workbook.write(excel_file_path) if updated
   end
+end
+
+def set_time_in_cell(worksheet, row, cell_index, date_value, cell_date)
+  cell = worksheet.add_cell(row.r - 1, cell_index)
+  cell.set_number_format('h:mm')
+  cell.change_contents(DateTime.strptime(cell_date.strftime('%Y/%m/%d') + ' ' + date_value, "%Y/%m/%d %H:%M:%S"))
 end
 
 data = read_entry_exit_csv
